489c7b10b628ae908d1df3e423e4d5896725fa25
CuriaX8 <CuriaX8@50e82c75-c798-0ba3-ebee-6d3882f1594b>
2010-09-23 04:37:52 +0000

    initial CCS project setup of RASDemo
    git-svn-id: http://rasware.googlecode.com/svn/trunk@14 50e82c75-c798-0ba3-ebee-6d3882f1594b

---
+++ Debug/RASDemo.pp
@@ -0,0 +1,26 @@
+# FIXED
+RASDemo.obj: ../RASDemo.c
+RASDemo.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/tms470/include/stdlib.h
+RASDemo.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/tms470/include/linkage.h
+RASDemo.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/tms470/include/ctype.h
+RASDemo.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/tms470/include/_isfuncdcl.h
+RASDemo.obj: C:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/tms470/include/_isfuncdef.h
+RASDemo.obj: C:/StellarisWare/inc/hw_types.h
+RASDemo.obj: C:/StellarisWare/driverlib/adc.h
+RASDemo.obj: C:/StellarisWare/driverlib/uart.h
+RASDemo.obj: C:/StellarisWare/driverlib/sysctl.h
+RASDemo.obj: C:/StellarisWare/utils/uartstdio.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/uart.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/init.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/encoder.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/linesensor.h
+RASDemo.obj: C:/StellarisWare/inc/hw_ints.h
+RASDemo.obj: C:/StellarisWare/inc/hw_memmap.h
+RASDemo.obj: C:/StellarisWare/driverlib/gpio.h
+RASDemo.obj: C:/StellarisWare/driverlib/pin_map.h
+RASDemo.obj: C:/StellarisWare/driverlib/interrupt.h
+RASDemo.obj: C:/StellarisWare/driverlib/timer.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/timer.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/motor.h
+RASDemo.obj: C:/StellarisWare/driverlib/pwm.h
+RASDemo.obj: C:/Users/MikeMay/working_dir/raslib/RASLib/include/RASLib/servo.h
