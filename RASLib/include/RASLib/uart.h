d2a6335f26276354690d1b1553b259b64293830a
supernic2002 <supernic2002@50e82c75-c798-0ba3-ebee-6d3882f1594b>
2010-11-20 00:57:02 +0000

    fixed PRINT_I(); removed \n from end of PRINT_?(). Instead add NL after PRINT_?():
    PRINT_I(myInt); NL;
    
    git-svn-id: http://rasware.googlecode.com/svn/trunk@47 50e82c75-c798-0ba3-ebee-6d3882f1594b

--- RASLib/include/RASLib/uart.h
+++ RASLib/include/RASLib/uart.h
@@ -17,15 +17,15 @@ extern "C"
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);		\
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);	\
         UARTStdioInit(0)                                // Initialize UART, port 0
-#define NL              UARTprintf("\n")          	// print a newline
-#define TAB             UARTprintf("\t")          	// print a tab
+#define NL              UARTprintf("\r\n")            	// print a newline
+#define TAB             UARTprintf("\t")             	// print a tab
 #define PRINT(s)        UARTprintf("%s", s)             // print a string
-#define PRINT_C(c)      UARTprintf("%s: %c\n", #c, c)   // print a char variable
-#define PRINT_D(d)      UARTprintf("%s: %d\n", #d, d)   // print a decimal variable
-#define PRINT_I(i)      UARTprintf("%s: %i\n", #i, i)   // print an integer variable
-#define PRINT_S(s)      UARTprintf("%s: %s\n", #s, s)   // print a string variable
-#define PRINT_U(u)      UARTprintf("%s: %u\n", #u, u)   // print an unsigned variable
-#define PRINT_X(x)      UARTprintf("%s: %x\n", #x, x)   // print a hexadecimal variable
+#define PRINT_C(c)      UARTprintf("%s: %c", #c, c)     // print a char variable
+#define PRINT_D(d)      UARTprintf("%s: %d", #d, d)     // print a decimal variable
+#define PRINT_I(i)      PRINT_D(i)   					// print an integer variable
+#define PRINT_S(s)      UARTprintf("%s: %s", #s, s)     // print a string variable
+#define PRINT_U(u)      UARTprintf("%s: %u", #u, u)     // print an unsigned variable
+#define PRINT_X(x)      UARTprintf("%s: %x", #x, x)     // print a hexadecimal variable
 
 #ifdef UART_BUFFERED
         #define UARTIsCharAvail()       UARTRxBytesAvail()
