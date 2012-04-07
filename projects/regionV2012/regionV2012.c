c8a5a7cc12e62e84726ac3cfca1a71bb4965845a
Christopher Haster <chaster@utexas.edu>
2012-04-07 06:25:13 -0500

    Added Corner Definitions and setup a prototype of a game plan

--- projects/regionV2012/regionV2012.c
+++ projects/regionV2012/regionV2012.c
@@ -8,17 +8,47 @@
 #include "RASLib/init.h"
 
 
+// Definitions of corners
+#define FLAG 0
+#define DIRECT 1 //couldn't remember name of this one
+#define LIGHT 2
+#define FAN 3
+#define TREE -1
+
+// Order of sources
+// The order indicates which one we want first
+#define SOURCE_1 LIGHT
+#define SOURCE_2 FAN
+#define SOURCE_3 DIRECT
+
+// Current location of robot
+signed char currentCorner = TREE;
+
+// These functions are up in the air at this point
 //
-//Follow Wall method?
+//void followWall(void) {
+//	//TODO figure out what exactly should go here
+//}
 //
-void followWall(void) {
-	//TODO figure out what exactly should go here
+//void moveForward(void) {
+//	//-128 to 127
+//	SetMotorPowers(127,127);
+//}
+//
+//
+
+//
+// Goes to location
+//
+void gotoCorner(dest) {
+	if (dest==TREE || dest==currentCorner) return; //return if invalid
+
+	//TODO move to robot to this corner
+
+	currentCorner = dest;
 }
 
-void moveForward(void) {
-	//-128 to 127
-	SetMotorPowers(127,127);
-}
+
 
 
 //
@@ -39,15 +69,26 @@ void init(void) {
 	InitializeMotors(false, false);
 }
 
+
 //
 //Run method
 //
 void run(void) {
-	//Put actual game code here
-	UARTprintf("Hello World\n");
+	signed char randoms = 3;
 
-	//Temp
-	moveForward();
+	//first when they are randomly on
+	for (;randoms>=0;randoms--) {
+		gotoCorner(nextSource());
+			//TODO charge for some time?
+		gotoCorner(FLAG);
+			//TODO discharge?
+	}
+
+	//then when they are all on
+	gotoCorner(SOURCE_1);
+		//TODO charge
+	gotoCorner(FLAG);
+		//TODO discharge
 }
 
 //
