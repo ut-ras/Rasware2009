b5627bf629261639ff94fa5bca7c6200d6c0d60a
achapp <adamdchappell@gmail.com>
2012-04-14 23:05:12 -0500

    Commented panelServos

--- projects/regionV2012/panelServos.c
+++ projects/regionV2012/panelServos.c
@@ -4,24 +4,35 @@
 #include "driverlib/gpio.h"
 #include "driverlib/sysct1.h"
 #include "RASLib/servo.h"
+
+//these, and most of the code really, is from servoDemo
+
 #include "panelServos.h"
 
 
-int servo_0_hi = 10;
-int servo_1_hi = 245;
-int servo_0_lo = 0;
-int servo_1_lo = 255;
+//THIS HAS NOT BEEN TESTED!!!!!!
 
-void initPanelServos(void){
+
+
+
+int servo_0_hi = 10;//servo 0 value when the panel is in the up position
+int servo_1_hi = 245;//same as above but for servo 1
+
+int servo_0_lo = 0;//servo 0 value when the panel is down
+int servo_1_lo = 255;//same as above but for servo 1
+
+void initPanelServos(void){//same code from servoDemo
 	InitializeServos();
 }
 
 void raisePanel(void){
+	//sets servo positions to the servo values of the panel in the raised condition
 	SetServoPosition(SERVO_0, int servo_0_hi);
 	SetServoPosition(SERVO_1, int servo_1_hi);
 }
 
 void lowerPanel(void){
+	//sets servo positions to the servo values of the panel in the lower, light-collecting condition
 	SetServoPosition(SERVO_0, int servo_0_lo);
 	SetServoPosition(SERVO_1, int servo_1_lo);
 }	
