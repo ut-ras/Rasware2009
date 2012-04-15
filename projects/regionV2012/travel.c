951796232c5b3ee489969826250f0c1a37f7b26c
Christopher Haster <chaster@utexas.edu>
2012-04-14 20:07:33 -0500

    Changed stuffs

--- projects/regionV2012/travel.c
+++ projects/regionV2012/travel.c
@@ -10,18 +10,6 @@ signed char currentCorner = TREE;
 // Array fro reading sensors
 signed char[8] sensors;
 
-=======
-unsigned short *readSensors(void) {
-  static unsigned short values[8] = {100,100,100,100,100,100,100,100};
-  //return an array of short values for the sensors
-<<<<<<< HEAD
-  return &values[0];  //values instead of &values?
-=======
-  return &values[0];    //values instead of &value?
->>>>>>> b672fc660f1d4c989f19fba8b90b724d4f6e6978
-}
->>>>>>> 81e400b7a9951044d918231d570b7207871aeba4
-
 void travelInit(void) {
 	//motors
 	InitializeMotors(false,false);
